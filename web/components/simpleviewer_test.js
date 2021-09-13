f4fb8bc5b4caa3afe81b089783e4ebada4b08425
fukakyon <fukasawa.yusuke@gmail.com>
2021-09-13 12:48:56 +0900

    move javascript code to *.js

--- web/components/simpleviewer_test.js
+++ web/components/simpleviewer_test.js
@@ -1,19 +1,4 @@
-/* Copyright 2014 Mozilla Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-"use strict";
+//"use strict";
 
 if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
   // eslint-disable-next-line no-alert
@@ -34,10 +19,14 @@ const DEFAULT_URL = "../../web/compressed.tracemonkey-pldi-09.pdf";
 // To test the AcroForm and/or scripting functionality, try e.g. this file:
 // var DEFAULT_URL = "../../test/pdfs/160F-2019.pdf";
 
-const SEARCH_FOR = ""; // try 'Mozilla';
+SEARCH_FOR = "Mozilla"; // try 'Mozilla';
 const SANDBOX_BUNDLE_SRC = "../../node_modules/pdfjs-dist/build/pdf.sandbox.js";
 
 const container = document.getElementById("viewerContainer");
+console.log(container);
+
+btn = document.getElementById("search");
+btn.addEventListener("click", on_search_click, false);
 
 const eventBus = new pdfjsViewer.EventBus();
 
@@ -65,7 +54,7 @@ const pdfViewer = new pdfjsViewer.PDFViewer({
   findController: pdfFindController,
   scriptingManager: pdfScriptingManager,
   enableScripting: true, // Only necessary in PDF.js version 2.10.377 and below.
-  maxCanvasPixels: 10
+  
 });
 pdfLinkService.setViewer(pdfViewer);
 pdfScriptingManager.setViewer(pdfViewer);
@@ -73,6 +62,7 @@ pdfScriptingManager.setViewer(pdfViewer);
 eventBus.on("pagesinit", function () {
   // We can use pdfViewer now, e.g. let's change default scale.
   pdfViewer.currentScaleValue = "page-width";
+  //pdfViewer.currentScaleValue = 0.5;
 
   // We can try searching for things.
   if (SEARCH_FOR) {
@@ -94,27 +84,30 @@ loadingTask.promise.then(function (pdfDocument) {
   pdfLinkService.setDocument(pdfDocument, null);
 });
 
-console.log("testtest");
-var container = document.getElementById('viewerContainer');
-var viewer = document.getElementById('viewer');
-var pdfViewer = new PDFViewer({ 
-  container: container,
-  viewer: viewer
-});
-pdfFindController = new PDFFindController({
-  pdfViewer: pdfViewer
-});
-pdfViewer.setFindController(pdfFindController);
-container.addEventListener('pagesinit', function () {
-    pdfViewer.currentScaleValue = 'page-width';                            
-});
-PDFJS.getDocument("").then(function (pdfDocument) {
-    pdfViewer.setDocument(pdfDocument);
-});
-
 function on_search_click(){
-  word = document.getElementById("search_word").value;
-  console.log(word);
-  pdfFindController.executeCommand("findhighlightallchange", { query: word });
-  //pdfViewer.update({scale: SCALE});
+  console.log("call on_search_click");
+  let search_word = document.getElementById("search_word").value;
+  console.log(search_word);
+
+  loadingTask.promise.then(function (pdfDocument) {
+    // find
+    // findhighlightallchange
+    // findagain
+    pdfFindController.executeCommand("find", {
+      query: search_word,
+      phraseSearch: true,
+      caseSensitive: false,
+      entireWord: false,
+      highlightAll: true,
+      findPrevious: true,
+      });
+  });
+
+  loadingTask.promise.then(function (pdfDocument) {
+    for (var i = 1; i <= pdfDocument.numPages; i++) {
+      pdfDocument.getPage(i).then(function(page) {
+        console.log(page.getTextContent());
+      })
+    }
+  });
 }
