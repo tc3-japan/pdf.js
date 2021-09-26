93c4898b676f955b32aa110ab39aa5fff632fb5a
fukakyon <fukasawa.yusuke@gmail.com>
2021-09-26 22:41:19 +0900

    fixed to change mouseup to dlclick

--- web/components/static/simpleviewer_test.js
+++ web/components/static/simpleviewer_test.js
@@ -23,10 +23,10 @@ select_sentence_button.addEventListener("click", on_select_sentence_button_click
 let clear_all_highlight_button = document.getElementById("clear_highlight");
 clear_all_highlight_button.addEventListener("click", on_clear_all_highlight_button_click, false);
 let viewer = document.getElementById("viewer");
-viewer.addEventListener("mouseup", on_mouse_up);
+viewer.addEventListener("dblclick", on_dblclick);
 var selected_sentnce_table = document.getElementById('selected_sentnce_table');
 
-function on_mouse_up(event) {
+function on_dblclick(event) {
   //console.log("on_mouse_up in");
   //console.log(event);
   //console.log(event.srcElement.textContent);
@@ -198,11 +198,8 @@ function on_clear_all_highlight_button_click(event) {
 
 // the remote pdf file url
 var pdf_url = '../uploads/tmp.pdf';
-var hilitepdf_url = '../uploads/tmp_highlighted.pdf';
-
 // reference to currently viewed PDF document sentences
 var sentences = null;
-
 // reference to matched sentences
 var matchedSentences = [];
 
@@ -211,11 +208,8 @@ $.extend({
      * file upload event handler, upload file to /upload POST method
      */
     uploadFile: function (e) {
-
         console.log(this.files);
-
         let file = this.files[0];
-
         var uploadUrl = '/upload';
         var xhr = new XMLHttpRequest();
         var fd = new FormData();
@@ -224,7 +218,6 @@ $.extend({
             if (xhr.readyState == 4 && xhr.status == 200) {
                 // Every thing ok, file uploaded
                 console.log(xhr.responseText); // handle response.
-
                 // Loading document.
                 const pdf_doc = pdfjsLib.getDocument({
                   url: "/static/uploads/tmp.pdf",
@@ -237,7 +230,6 @@ $.extend({
                   pdfViewer.setDocument(pdfDocument);
                   console.log("open pdf");
                 });
-
                 /**
                  * extract sentences from PDF
                  */
@@ -245,18 +237,12 @@ $.extend({
                     url: '/extract_sentences',
                     contentType: "application/json",
                 }).done(function(results) {
-
                     // no matched sentences when new pdf is loaded
                     matchedSentences = [];
-
                     // reference returned sentences from parsing the PDF
                     sentences = results;
-
                     console.log('results');
                     console.log(results);
-                    $('#table').bootstrapTable();
-                    $('#table').bootstrapTable('load', []);
-                    $('#table').bootstrapTable('resetView');
                 });
 
             }
@@ -265,154 +251,11 @@ $.extend({
         xhr.send(fd);
     },
 
-    /**
-     * PDF is loaded handling function
-     */
-    pdfloaded: function () {
-        console.log('pdfviewer-iframe loaded');
-
-        setTimeout(function() {
-
-            $('#pdfviewer-div').show();
-
-            // reference pdf viewer
-            var iframe = window.frames['pdfviewer-iframe']
-            var idoc = iframe.contentDocument || iframe.contentWindow.document; // ie compatibility
-
-            $("#pdfviewer-iframe").contents().find(".textLayer").mouseup(function() {
-                console.log('*** mouseup');
-
-                var text = idoc.getSelection().toString();
-
-                console.log('   idoc.getSelection()', idoc.getSelection());
-                console.log('   text', text);
-
-                // referenced to the clicked word
-                var clickedWord = idoc.getSelection().anchorNode.nodeValue;
-                console.log('clickedWord', clickedWord);
-
-                // highlight clicked word if present when no dragged over text
-                if (text.trim() === '' && clickedWord !== '') {
-
-                    // set the clicked word as searched text
-                    $('.search-input').val(clickedWord);
-
-
-                    // highlight sentences by searching clicked word in pdf viewer
-                    $.highlightSentenceByClickedWordInPDFViewer(idoc, clickedWord);
-
-
-                    // select sentences by clicked word
-                    var matches = $.selectSentencesByClickedWord(clickedWord);
-                    matchedSentences = matchedSentences.concat(matches);
-
-
-                    // show selected sentences in sentences in grid
-                    var table = $('#table');
-                    $.showSentenceByClickedWordInTable(table, matchedSentences);
-                }
-            })
-
-        }, 3000);
-    },
-
-
-    /**
-     * Select sentences by keyword
-     */
-    selectSentencesByClickedWord: function(clickedWord) {
-        // select sentences based on clicked word
-        var selectedSentences = [];
-        sentences.forEach(function(sentence) {
-            if (sentence.sentence.indexOf(clickedWord) > -1) {
-                sentence.clickedWord = clickedWord;
-                selectedSentences.push(sentence);
-            }
-        })
-        console.log('selectedSentences', selectedSentences);
-        return selectedSentences;
-    },
-
-
-    /**
-     * Show sentence by clicked word in table
-     */
-    showSentenceByClickedWordInTable: function(table, selectedSentences) {
-        $('#table').bootstrapTable();
-        $('#table').bootstrapTable('load', selectedSentences);
-        $('#table').bootstrapTable('resetView');
-    },
-
-
-    /**
-     * Highlight sentence by clicked word in pdf viewer
-     */
-    highlightSentenceByClickedWordInPDFViewer: function(idoc, clickedWord) {
-    
-        // search forward direction
-        var forwardSegments = [];
-        var sentenceWords = [];
-        var hasMatchedBefore = false;
-        $(idoc).find('div.textLayer>div').toArray().forEach(function(el, idx) {
-            var thisWord = $(el).html();
-            var hasMatchedClickedWord = thisWord === clickedWord;
-            if (hasMatchedBefore || hasMatchedClickedWord) {
-                console.log('thisWord', thisWord, 'idx', idx);
-                sentenceWords.push(thisWord);
-                $(el).css('background-color', 'red').css('color', 'white');
-                hasMatchedBefore = true;
-            }
-
-            var hasEndOfSentence = thisWord.indexOf('.') > -1 || thisWord.indexOf('?') > -1 || thisWord.indexOf('!') > -1;
-            if (hasEndOfSentence && hasMatchedBefore) {
-                hasMatchedBefore = false;
-                forwardSegments.push(sentenceWords.join(' '));
-                sentenceWords = [];
-            }
-        })
-
-        // now do a backward search
-        var backwardSegments = [];
-        var sentenceWords = [];
-        var hasMatchedBefore = false;
-        $(idoc).find('div.textLayer>div').toArray().reverse().forEach(function(el, idx) {
-            var thisWord = $(el).html();
-            var hasMatchedClickedWord = thisWord === clickedWord;
-            var hasEndOfSentence = thisWord !== clickedWord && (thisWord.indexOf('.') > -1 || thisWord.indexOf('?') > -1 || thisWord.indexOf('!') > -1);
-            if (!hasEndOfSentence && (hasMatchedBefore || hasMatchedClickedWord)) {
-                sentenceWords.push(thisWord);
-                $(el).css('background-color', 'red').css('color', 'white');
-                hasMatchedBefore = true;
-            }
-            if (hasEndOfSentence && hasMatchedBefore) {
-                backwardSegments.push(sentenceWords.reverse().join(' '));
-                hasMatchedBefore = false;
-                sentenceWords = [];
-            }
-        })
-    },
-
 })
 
 $(document).ready(function() {
-
     // define file upload event handler
     $('input[type="file"]').change($.uploadFile);
-
-    // permanently highlight sentences
-    $('#search-selected-button').click(function () {
-        $.ajax({
-            url: '/highlight_sentences',
-            method: 'POST',
-            contentType: "application/json",
-            data: JSON.stringify($('#table').bootstrapTable('getSelections'))
-        }).done(function(results) {
-
-            var uploadedPdfUrl = hilitepdf_url + '?run=' + parseInt(Math.random()*500000);
-
-            $('#pdfviewer-div').empty().hide().append('<iframe id="pdfviewer-iframe" src="/static/ViewerJS/index.html?title=' + pdfViewerCustomizations.title + '#' + uploadedPdfUrl + '" width=100% height=500 allowfullscreen webkitallowfullscreen onload="$.pdfloaded()"></iframe>');
-        });
-    })
 });
 
 
